b9bd616618f2260fbd8579a933eea7eaae35302a
Sergittko <derov180214@gmail.com>
2023-06-27 12:44:45 +0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  res.status(200).send("test 4");
+});
+
+app.listen(3000);
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
