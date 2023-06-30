e59f4bca08a8fd4269fc6a525fd65d4fd75aa9fb
Sergittko <derov180214@gmail.com>
2023-06-30 13:01:20 +0300

    refactor: use async/await instead of promise chaining

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,54 +4,48 @@ const handleError = (res, error) => {
   res.status(500).json({ error });
 };
 
-export const getUsers = (req, res) => {
-  User.find()
-    .then((users) => {
-      res.status(200).json(users);
-    })
-    .catch((err) => {
-      handleError(res, err);
-    });
+export const getUsers = async (req, res) => {
+  try {
+    let users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    handleError(res, err);
+  }
 };
 
-export const getUserById = (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => {
-      res.status(200).json(user);
-    })
-    .catch((err) => {
-      handleError(res, err);
-    });
+export const getUserById = async (req, res) => {
+  try {
+    let user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    handleError(res, err);
+  }
 };
 
-export const postNewUser = (req, res) => {
-  const user = new User(req.body);
-  user
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((err) => {
-      handleError(res, err);
-    });
+export const postNewUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    let result = await user.save();
+    res.status(201).json(result);
+  } catch (err) {
+    handleError(res, err);
+  }
 };
 
-export const changeUserData = (req, res) => {
-  User.findByIdAndUpdate(req.params.id, req.body)
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      handleError(res, err);
-    });
+export const changeUserData = async (req, res) => {
+  try {
+    let result = await User.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    handleError(res, err);
+  }
 };
 
-export const deleteUser = (req, res) => {
-  User.findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      handleError(res, err);
-    });
+export const deleteUser = async (req, res) => {
+  try {
+    let result = await User.findByIdAndDelete(req.params.id);
+    res.status(200).json(result);
+  } catch (err) {
+    handleError(res, err);
+  }
 };
