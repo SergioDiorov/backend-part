b25a595aaf577494a53b8e40db02a41721a4c6ac
Sergittko <derov180214@gmail.com>
2023-07-13 15:32:59 +0300

    feat: add CORS headers to allow cross-origin requests

--- routes/auth-routes.ts
+++ routes/auth-routes.ts
@@ -3,6 +3,15 @@ import { signInUser, signUpUser } from '../controllers/auth-controller';
 
 const authRouter = express.Router();
 
+authRouter.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 authRouter.post('/authUser/signup', signUpUser);
 authRouter.post('/authUser/signin', signInUser);
 
