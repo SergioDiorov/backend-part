6d2c0e15ff54bc4ba30cdad1e44e2c6d897e56b3
Sergittko <derov180214@gmail.com>
2023-08-02 13:19:09 +0300

    refactor: update unit test for cheack admin middleware

--- __tests__/unit/checkAdminMiddleware.test.ts
+++ __tests__/unit/checkAdminMiddleware.test.ts
@@ -1,72 +1,79 @@
-import request from 'supertest';
 import { Server } from "http";
+import { NextFunction, Request, Response } from 'express';
 
+import { checkAdmin } from './../../middlewares/check-admin-middleware';
 import app from "server";
-import User from "models/user";
-import * as tokenService from 'service/token-service';
+import { ObjectId } from 'mongodb';
+
+interface RequestWithUser extends Request {
+  user?: {
+    id: ObjectId | string;
+  };
+}
+
+let server: Server;
+let mockRequest: Partial<RequestWithUser>;
+let mockResponse: Partial<Response>;
+let nextFunction: NextFunction = jest.fn();
+
+beforeAll((done) => {
+  server = app.listen(() => {
+    done();
+  });
+
+  mockRequest = {};
+  mockResponse = {
+    json: jest.fn(),
+    status: jest.fn().mockReturnThis()
+  };
+});
+
+afterAll((done) => {
+  server.close(() => {
+    done();
+  });
+});
 
 describe("Check admin middleware", () => {
-  let server: Server;
-
-  beforeAll((done) => {
-    server = app.listen(() => {
-      done();
-    });
-  });
-
-  afterAll((done) => {
-    server.close(() => {
-      done();
-    });
-  });
-
-
-  it('should return "SUCCESS" message if user is admin', async () => {
-    const fakeUser = {
-      userName: 'checkAdmin',
-      email: 'checkAdmin@test.com',
-      password: 'password',
-      isAdmin: true
+  it('should return "No user data provided" error if user is not admin', async () => {
+    const expectedResponse = {
+      message: "No user data provided",
+      code: 403
     };
-    const savedUser = await User.create(fakeUser);
-
-    const tokens = tokenService.generateTokens({
-      email: savedUser.email,
-      id: savedUser._id,
-    });
-
-    const response = await request(app)
-      .get('/users')
-      .set('Authorization', `Bearer ${tokens.accessToken}`);
-
-    await User.deleteOne({ _id: savedUser._id });
-    expect(response.body.message).toBe("SUCCESS")
-    expect(response.status).toBe(200)
+    await checkAdmin(mockRequest as Request,
+      mockResponse as Response,
+      nextFunction);
+    expect(mockResponse.json).toBeCalledWith(expectedResponse);
   });
 
   it('should return "Access forbidden" error if user is not admin', async () => {
-    const fakeUser = {
-      userName: 'checkAdmin2',
-      email: 'checkAdmin2@test.com',
-      password: 'password',
-      isAdmin: false
-    };
-    const savedUser = await User.create(fakeUser);
+    mockRequest = {
+      user: {
+        id: new ObjectId()
+      }
+    }
 
-    const tokens = tokenService.generateTokens({
-      email: savedUser.email,
-      id: savedUser._id,
-    });
-
-    const response = await request(app)
-      .get('/users')
-      .set('Authorization', `Bearer ${tokens.accessToken}`);
-
-    await User.deleteOne({ _id: savedUser._id });
-    expect(response.body).toEqual({
-      message: 'Access forbidden',
+    const expectedResponse = {
+      message: "Access forbidden",
       code: 403
-    });
-    expect(response.status).toBe(403);
+    };
+    await checkAdmin(mockRequest as Request,
+      mockResponse as Response,
+      nextFunction);
+    expect(mockResponse.json).toBeCalledWith(expectedResponse);
+  });
+
+  it('should return success', async () => {
+    mockRequest = {
+      user: {
+        id: "64b1653b2e6c7d54e2694eea"
+      }
+    }
+
+    await checkAdmin(mockRequest as Request,
+      mockResponse as Response,
+      nextFunction);
+
+    expect(nextFunction).toBeCalledTimes(1);
   });
 })
