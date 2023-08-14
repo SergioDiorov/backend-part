939d546710f64b4fca41fd587fa7f01c30bdbb10
Sergittko <derov180214@gmail.com>
2023-08-14 18:54:03 +0300

    feat: add new search endpoints for profile

--- routes/profiles-routes.ts
+++ routes/profiles-routes.ts
@@ -2,15 +2,27 @@ import express from 'express';
 
 import { authenticateToken } from 'middlewares/auth-middleware';
 import {
+  getProfiles,
+  getProfileByName,
+  getAdultProfiles,
+  getCountriesList,
+  getCitiesList,
   addProfile,
   changeProfileData,
   deleteProfile,
-  getProfiles
+  getProfilesByCountry,
+  getProfilesByCity,
 } from 'controllers/profile-controller';
 
 const profilesRouter = express.Router();
 
 profilesRouter.get('/:userId', authenticateToken, getProfiles);
+profilesRouter.get('/searchByName/:userId', authenticateToken, getProfileByName);
+profilesRouter.get('/searchByCountry/:userId/:country', authenticateToken, getProfilesByCountry);
+profilesRouter.get('/searchByCity/:userId/:city', authenticateToken, getProfilesByCity);
+profilesRouter.get('/searchAdults/:userId', authenticateToken, getAdultProfiles);
+profilesRouter.get('/searchCountriesList/:userId', authenticateToken, getCountriesList);
+profilesRouter.get('/searchCitiesList/:userId', authenticateToken, getCitiesList);
 profilesRouter.post('/:userId', authenticateToken, addProfile);
 profilesRouter.patch('/:profileId', authenticateToken, changeProfileData);
 profilesRouter.delete('/:profileId', authenticateToken, deleteProfile);
